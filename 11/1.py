30391dbb9f795d480c5c70599c919cb6e5f5e964
bernizhel <detrop.1995@gmail.com>
2022-11-28 22:04:48 +0300

    feat(11): update 11th work

--- 11/1.py
+++ 11/1.py
@@ -1,113 +1,144 @@
 # -*- coding: utf-8 -*-
 
-import httpx
-from retrying import retry
-from loguru import logger
-
-from urllib.parse import urlparse
-from threading import Thread
 import json
 import os
-
+import re
+from threading import Thread
 from tkinter import *
 from tkinter import messagebox
+from urllib.parse import urlparse
+
+import httpx
+from bs4 import BeautifulSoup
+from loguru import logger
 
 
-def tkinter_thread_function(func):
-  def _(*k, **kw):
-    def __():
-      thread = Thread(target=func, args=k, kwargs=kw)
-      thread.start()
-      return thread
+def threaded(f):
+    def _(*k, **kw):
+        thread = Thread(target = f, args = k, kwargs = kw)
+        thread.start()
+        return thread
 
-    return __
-
-  return _
+    return _
 
 
-def get_path(filename):
-  return os.path.realpath(os.path.join(__file__, '..', filename))
+def generate_saved_repo_path(url):
+    splitted_parts = list(map(str, urlparse(url).path.split('/')))
+    filename = splitted_parts[-2] + '__' + splitted_parts[-1]
+    filename += '.json'
+
+    return os.path.realpath(os.path.join(__file__, '..', filename))
 
 
-def generate_path(url):
-  filename = urlparse(url).path.split('/')[-1]
-  filename += '.json'
-  
-  return get_path(filename)
+KEYS = ['company', 'created_at', 'email', 'id', 'name', 'url']
 
 
-@retry
+def extract_keys(data, keys):
+    return dict((key, data[key]) for key in keys if key in data)
+
+
 def get_data(url):
-  logger.debug(f'Fetching the data of {url}...')
+    global KEYS
 
-  response = httpx.get(url)
-  data = response.json()
+    logger.debug(f'Fetching the data of {url}...')
 
-  logger.debug(f'Fetched data of {url} successfully.')
+    response = httpx.get(url)
+    data = response.json()
+    data = extract_keys(data, KEYS)
 
-  return data
+    logger.debug(f'Got {url} successfully.')
+
+    return data
 
 
 def save_data(data, filepath):
-  with open(filepath, 'w') as data_file:
-    json.dump(data, data_file, indent=2)
+    with open(filepath, 'w') as data_file:
+        json.dump(data, data_file, indent = 2)
 
 
-REPOS = [
-  'https://api.github.com/repos/kubernetes/kubernetes',
-  'https://api.github.com/repos/apache/spark',
-  'https://api.github.com/repos/Microsoft/vscode',
-  'https://api.github.com/repos/NixOS/nixpkgs',
-  'https://api.github.com/repos/rust-lang/rust',
-  'https://api.github.com/repos/firehol/blocklist-ipsets',
-  'https://api.github.com/repos/openshift/origin',
-  'https://api.github.com/repos/ansible/ansible',
-  'https://api.github.com/repos/Automattic/wp-calypso',
-  'https://api.github.com/repos/dotnet/corefx',
-]
+BASE_URL = 'https://habr.com/ru/post/453444/'
+
+GITHUB_URL_REGEXP = r'(https://)(github.com/)([-\w._]+/[-\w._]+/?)'
 
 
-@tkinter_thread_function
+def convert_www_to_api(github_url):
+    global GITHUB_URL_REGEXP
+
+    return re.sub(GITHUB_URL_REGEXP, r'\1api.\2repos/\3', github_url)
+
+
+def get_repos():
+    global BASE_URL
+    global GITHUB_URL_REGEXP
+
+    repos = []
+
+    soup = BeautifulSoup(httpx.get(BASE_URL).text, 'lxml')
+
+    for link in soup.find_all():
+        condition = link is not None and 'href' in link.attrs and \
+                    re.fullmatch(GITHUB_URL_REGEXP, link.get('href')) and \
+                    link.get('href') not in repos
+
+        if condition:
+            repos.append(link.get('href'))
+
+    for i in range(len(repos)):
+        repos[i] = convert_www_to_api(repos[i])
+
+    logger.debug(f'Found the following amount of repo links: {len(repos)}.')
+
+    return repos
+
+
 def fetch_repo(entry):
-  url = REPOS[int(entry.get()[-1])]
+    @threaded
+    def _():
+        repos = get_repos()
 
-  filepath = generate_path(url)
+        url = repos[int(entry.get())]
 
-  data = get_data(url)
+        filepath = generate_saved_repo_path(url)
 
-  save_data(data, filepath)
+        data = get_data(url)
 
-  messagebox.showinfo(title='Success', message='Saved the data into ' + filepath)
+        save_data(data, filepath)
+
+        messagebox.showinfo(title = 'Success',
+                            message = 'Saved the data into ' + filepath)
+
+    return _
 
 
 def initialize_app():
-  main_window = Tk()
-  main_window.minsize(400, 50)
-  main_window.title('Retrieve the repo\'s data')
-  
-  main_window.grid_rowconfigure(0, weight=1)
-  main_window.grid_rowconfigure(1, weight=1)
-  main_window.grid_rowconfigure(2, weight=1)
-  main_window.grid_columnconfigure(0, weight=1)
+    main_window = Tk()
+    main_window.minsize(400, 50)
+    main_window.title('Retrieve the repo\'s data')
 
-  label_number = Label(text='Enter your gradebook\'s number:')
-  label_number.grid(row=0, column=0)
+    main_window.grid_rowconfigure(0, weight = 1)
+    main_window.grid_rowconfigure(1, weight = 1)
+    main_window.grid_rowconfigure(2, weight = 1)
+    main_window.grid_columnconfigure(0, weight = 1)
 
-  entry_number = Entry(main_window, width=6)
-  entry_number.grid(row=1, column=0)
+    label_number = Label(
+        text = 'Enter # number of repo that you want to fetch:')
+    label_number.grid(row = 0, column = 0)
 
-  button_get = Button(
-    text='Get it',
-    command=fetch_repo(entry_number)
-  )
-  button_get.grid(row=2, column=0)
+    entry_number = Entry(main_window, width = 6)
+    entry_number.grid(row = 1, column = 0)
 
-  main_window.mainloop()
+    button_get = Button(
+        text = 'Get it',
+        command = fetch_repo(entry_number)
+    )
+    button_get.grid(row = 2, column = 0)
+
+    main_window.mainloop()
 
 
 def main():
-  initialize_app()
+    initialize_app()
 
 
 if __name__ == '__main__':
-  main()
+    main()
